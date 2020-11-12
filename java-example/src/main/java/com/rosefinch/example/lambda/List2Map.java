d7b11625b85e7eded18511bf5a8e84ab381d8d65
huangyucai2 <huangyucai2@yy.com>
2020-11-12 12:05:34 +0800

    feat: list2Map

---
+++ java-example/src/main/java/com/rosefinch/example/lambda/List2Map.java
@@ -0,0 +1,36 @@
+package com.rosefinch.example.lambda;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * @author York.Hwang
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class List2Map {
+
+    public static void main(String[] args) {
+        List<Apple> appleList = Arrays.asList(
+                new Apple("1","a"),
+                new Apple("2","c"),
+                new Apple("3","d"),
+                new Apple("1","e"));
+        Map<String,Apple> appleMap = appleList.stream().collect(Collectors.toMap(Apple::getId, a -> a,(k1, k2)->k1));
+
+
+        System.out.println(appleMap);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Apple{
+        private String id;
+        private String color;
+    }
+}
